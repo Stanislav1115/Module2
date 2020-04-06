d497c5abffc37f7426f9da8b4ee24f0f3b150706
Stanislav1115 <62393265+Stanislav1115@users.noreply.github.com>
2020-04-06 21:55:26 +0300

    Add files via upload

---
+++ ConsoleApp2/ConsoleApp2/Classes/Tickets.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2.Classes
+{
+    class Tickets
+    {
+        private string hotellclass;
+        private string country;
+        private double price;
+        private string pitanie;
+        public string Country
+        {
+            get { return country; }
+            set { country = value; }
+        }
+
+        public string HotellClass
+        {
+            get { return hotellclass; }
+            set { hotellclass = value; }
+        }
+
+        public double Price
+        {
+            get { return price; }
+            set { price = value; }
+        }
+
+        public string Pitanie
+        {
+            get { return pitanie; }
+            set { pitanie = value; }
+        }
+
+    }
+}
